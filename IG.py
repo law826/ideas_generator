0510968c117e94327d50b94eebd2c736ee085dc9
Lawrence Ngo <law826@gmail.com>
2013-07-02 13:17:24 -0400

    Revert "todo list"
    This reverts commit 14b1ad72d713ca0e3344de13a3c4687aea4eda64.

--- IG.py
+++ IG.py
@@ -7,7 +7,6 @@ Created by VL and LN on 2012-08-15.
 Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 
 To do list:
-[] build a way to focus on a particular term (for problem solving)
 [] support for multiple users
 [] implement smart correlations
 [] set path for save files within gui
